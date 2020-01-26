2723af1331446fb2347eb72e87deff8db0afc379
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-01-26 14:17:23 +0300

    Add files via upload

---
+++ FALLEN KINGDOM/game/FK_ENGINE.c
@@ -0,0 +1,42 @@
+// main engine  for maps 
+#include <"wchar.h">
+#include <"windows.h">
+#include <"string.h">
+
+
+struct triggers
+{
+    char 
+	const string name_of_constant;
+
+	int  save_of_trigger () {
+         for (int  i = 0 ; i<1; i++ )
+         {
+         	const string name_of_constant = mak_file_const ; 
+         }      
+    
+      return 0;		
+	}
+};
+
+
+
+
+
+
+int mainWindow(int argc, char const *argv[])
+{  
+	int  width= 1900;
+	int  height= 680;
+
+
+
+	/* code */
+	return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
