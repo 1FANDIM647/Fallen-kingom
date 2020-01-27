3940658c4b1c73eea87277b5c92f827d3729e6ec
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-01-27 20:48:45 +0300

    Add files via upload

---
+++ FK_ENGINE.c
@@ -0,0 +1,60 @@
+// main engine  for maps 
+#include <"wchar.h">
+#include <"windows.h">
+
+
+// we create triggers
+struct triggers
+{
+    char mak_file_const;
+	char name_of_constant;
+
+	// function saving of trigger
+
+	char save_of_trigger (char name_of_constant;) {
+
+         for (int  i = 0 ; i<1; i++ )
+         {
+         	// name of trigger will be written "in mak_file_const"  variable
+             name_of_constant = mak_file_const ; 
+         }      
+    
+      return  mak_file_const;		
+	}
+
+// function deleting trigger
+	char delete_of_trigger (char name_of_constant;) {
+         void label_Deleting_of_trigger  () {
+            char delete_of_trigger (char name_of_constant;)
+            {
+
+            	delete ( name_of_constant);
+            }
+
+        } 
+    return name_of_constant;           
+
+	}
+};
+
+
+
+
+
+
+int mainWindow(int argc, char const *argv[])
+{  
+	int  width= 1900;
+	int  height= 680;
+
+    
+
+	/* code */
+	return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+	int  mainWindow ();
+	return 0;
+}
